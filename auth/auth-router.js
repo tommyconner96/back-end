6455122880e90b9c76ed013527f76c9d4082ce16
unknown <tommyconner96@gmail.com>
2020-07-29 18:57:17 -0400

    typo

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -40,7 +40,7 @@ router.post('/login', (req, res, next) => {
 				const token = createToken(user)
 
 				// change to this when deployed. on localhost it breaks it.
-				res.cookie("token", token, { sameSite: 'none', secure: true, httpOnly: true })
+				res.cookie("token", token, { SameSite: 'none', secure: true, httpOnly: true })
 				// res.cookie("token", token)
 				res.status(200).json({ message: `Hi ${user.username}! have a token:`, token, user_id: user.id })
 			} else {
